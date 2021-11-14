ea0b910a37396ea9f128be2a537e7d171175bffb
Marcelo <snaker.extreme.15@gmail.com>
2021-11-14 19:17:20 -0300

    [master] <Marcelo> Preparing the app to deploy

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from settings.production_settings import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base_app.urls')),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT)
