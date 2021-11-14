c12dcd5652bad6f93eeb65e6b0463228b03881be
Marcelo <snaker.extreme.15@gmail.com>
2021-11-14 06:30:48 -0300

    [master] <Marcelo> Preparing the app to deploy

--- ToDoList/settings/production_settings.py
+++ ToDoList/settings/production_settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'base_app//static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
