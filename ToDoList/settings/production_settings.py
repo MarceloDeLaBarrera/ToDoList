3b403e1561c2351d577926fb46a129640355b4b3
Marcelo <snaker.extreme.15@gmail.com>
2021-11-14 20:16:31 -0300

    [master] <Marcelo> Preparing the app to deploy

--- ToDoList/settings/production_settings.py
+++ ToDoList/settings/production_settings.py
@@ -134,8 +134,8 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),
-                    os.path.join(BASE_DIR, 'base_app//static'))
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),
+# os.path.join(BASE_DIR, 'base_app//static'))
 
 
 # Default primary key field type
