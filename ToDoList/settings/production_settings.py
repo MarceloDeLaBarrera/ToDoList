37193272ecd5c1a45feacca76bbd867803b88c2a
Marcelo <snaker.extreme.15@gmail.com>
2021-11-14 09:22:19 -0300

    [master] <Marcelo> Preparing the app to deploy

--- ToDoList/settings/production_settings.py
+++ ToDoList/settings/production_settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = str(os.environ.get('DEBUG')) == "0"
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://to-do-list-marcelo.herokuapp.com/']
 
 
 # Application definition
