a521e76dc9c4e12090073e3f06066c337eb5205f
Marcelo <snaker.extreme.15@gmail.com>
2021-11-14 21:59:46 -0300

    [master] <Marcelo> Preparing the app to deploy

--- base_app/migrations/0001_initial.py
+++ base_app/migrations/0001_initial.py
@@ -17,12 +17,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Task',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('tittle', models.CharField(max_length=220)),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=220)),
                 ('description', models.TextField(blank=True, null=True)),
                 ('complete', models.BooleanField(default=False)),
                 ('created_date', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(blank=True, null=True,
+                 on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'verbose_name': 'Tasks',
