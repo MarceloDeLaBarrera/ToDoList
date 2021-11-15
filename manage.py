b93cb0c78593048b1ffeae910ca1c52488daadfb
Marcelo <snaker.extreme.15@gmail.com>
2021-11-15 00:33:53 -0300

    [master] <Marcelo> Preparing the app to deploy and local

--- manage.py
+++ manage.py
@@ -7,10 +7,10 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    if os.environ.get("DJANGO_SETTINGS_MODULE") == "ToDoList.settings.settings":
-        dotenv.read_dotenv()
+    # if str(os.environ.get("DJANGO_SETTINGS_MODULE")) == "ToDoList.settings.settings":
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'ToDoList.settings.production_settings')
+                          'ToDoList.settings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
